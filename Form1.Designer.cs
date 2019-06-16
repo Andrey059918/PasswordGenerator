35dcb4704b66fce14958ab2c96859dde831e58af
Андрей Кокорин <akokorin99@gmail.com>
2019-06-16 14:06:25 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PasswordGenerator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(15, 15);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(400, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.Color.White;
+			this.textBox2.Location = new System.Drawing.Point(15, 45);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(400, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BackColor = System.Drawing.Color.White;
+			this.textBox3.Location = new System.Drawing.Point(15, 75);
+			this.textBox3.Margin = new System.Windows.Forms.Padding(5);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(400, 20);
+			this.textBox3.TabIndex = 2;
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.Color.White;
+			this.textBox4.Location = new System.Drawing.Point(15, 135);
+			this.textBox4.Margin = new System.Windows.Forms.Padding(5);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(400, 20);
+			this.textBox4.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(430, 170);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "MD5 Password Generator";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace PasswordGenerator
+{
+	public partial class Form1 : Form
+	{
+		MD5Cng md5hasher;
+		public Form1()
+		{
+			InitializeComponent();
+			md5hasher = new MD5Cng();
+			SetTextBoxEvents(textBox1, "Адрес или название сайта");
+			SetTextBoxEvents(textBox2, "Телефон, логин или E-mail");
+			SetTextBoxEvents(textBox3, "Контрольное слово или фраза");
+			MakeHash();
+			textBox1.Select(0,0);
+		}
+
+
+		private void SetTextBoxEvents(TextBox textBox, String defaultText)
+		{
+			textBox.Text = defaultText;
+			textBox.ForeColor = Color.FromArgb(75,75,75);
+			textBox.KeyDown += (sender, args) =>
+			{
+				if (args.KeyCode == Keys.Back)
+				{
+					if (textBox.TextLength <2)
+					{
+						textBox.Text = defaultText;
+						textBox.ForeColor = Color.FromArgb(75, 75, 75);
+					}
+				}
+				else if (textBox.Text == defaultText)
+				{
+					textBox.Text = "";
+					textBox.ForeColor = Color.Black;
+				}
+				MakeHash();
+			};
+		}
+		
+
+		private void MakeHash() {
+			var md5hash = md5hasher.ComputeHash(Encoding.UTF8.GetBytes(textBox1.Text + textBox2.Text ));
+			md5hash = md5hasher.ComputeHash(md5hash.Concat(Encoding.UTF8.GetBytes(textBox3.Text)).ToArray());
+			String result="";
+			md5hash.ToList().ForEach(a => result += a.ToString("x2"));
+			textBox4.Text = result;
+		}
+
+	}
+}
