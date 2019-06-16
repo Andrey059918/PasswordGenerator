6b2e2fc8c9a6a1e521878467eae35c39a225fb74
Андрей Кокорин <akokorin99@gmail.com>
2019-06-16 14:53:24 +0300

    1.0.2 - Added upper letters to passwords

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace PasswordGenerator
 	public partial class Form1 : Form
 	{
 		MD5Cng md5hasher;
+		Random randomizer;
 		public Form1()
 		{
 			InitializeComponent();
@@ -22,19 +23,19 @@ namespace PasswordGenerator
 			SetTextBoxEvents(textBox2, "Телефон, логин или E-mail");
 			SetTextBoxEvents(textBox3, "Контрольное слово или фраза");
 			MakeHash();
-			textBox1.Select(0,0);
+			textBox1.Select(0, 0);
 		}
 
 
 		private void SetTextBoxEvents(TextBox textBox, String defaultText)
 		{
 			textBox.Text = defaultText;
-			textBox.ForeColor = Color.FromArgb(75,75,75);
+			textBox.ForeColor = Color.FromArgb(75, 75, 75);
 			textBox.KeyDown += (sender, args) =>
 			{
 				if (args.KeyCode == Keys.Back)
 				{
-					if (textBox.TextLength <2)
+					if (textBox.TextLength < 2)
 					{
 						textBox.Text = defaultText;
 						textBox.ForeColor = Color.FromArgb(75, 75, 75);
@@ -48,18 +49,29 @@ namespace PasswordGenerator
 				MakeHash();
 			};
 		}
-		
 
-		private void MakeHash() {
-			var md5hash = md5hasher.ComputeHash(Encoding.UTF8.GetBytes(textBox1.Text + textBox2.Text ));
+
+		private void MakeHash()
+		{
+			var md5hash = md5hasher.ComputeHash(Encoding.UTF8.GetBytes(textBox1.Text + textBox2.Text));
 			md5hash = md5hasher.ComputeHash(md5hash.Concat(Encoding.UTF8.GetBytes(textBox3.Text)).ToArray());
-			String result="";
-			md5hash.ToList().ForEach(a => result += a.ToString("x2"),EndShift:8);
-			textBox4.Text = result;
-			md5hash.ToList().ForEach(a => result += a.ToString("x2"),StartShift: 8);
-			textBox5.Text = result;
+			String result = "";
+			md5hash.ToList().ForEach(a => result += a.ToString("x2"));
+			randomizer = new Random(textBox3.Text.ToList().Sum(a => a));
+			result.ForEach(a =>
+			{
+				if (randomizer.Next(2) == 1)
+				{
+					result += Char.ToUpper(a);
+				}
+				else
+				{
+					result += a;
+				}
+			});
+			textBox4.Text = String.Concat(result.Skip(32).Take(16));
+			textBox5.Text = String.Concat(result.Skip(32));
 		}
-
 	}
 	public static class Extensions
 	{
@@ -78,4 +90,4 @@ namespace PasswordGenerator
 			}
 		}
 	}
-	}
+}
